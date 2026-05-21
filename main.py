bba782b53628a404a4afcfd3ed8e6742d39e0215
Kush Sethia <kushsethia09@gmail.com>
2026-05-20 20:17:51 -0400

    clean up

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import src.camera
 import cv2
+import src.pose
 
 running = True
+pose = src.pose.pose()
 
 while True:
     cam = src.camera.startVideo()
@@ -9,8 +11,10 @@ while True:
         frame = cam.get_frame()
         if frame is None:
             break
+        frame, landmarks = pose.process(frame)
         cv2.imshow("Video", frame)
         cv2.waitKey(1)
+        #draw
 
         if cv2.getWindowProperty("Video", cv2.WND_PROP_VISIBLE) < 1:
             running = False
---
+++ src/pose.py
@@ -0,0 +1,24 @@
+import mediapipe as mp
+import cv2
+
+class pose():
+    def __init__(self):
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose()
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def process(self, frame, draw=True):
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.pose.process(rgb)
+        landmarks = None
+
+        if results.pose_landmarks:
+            landmarks = results.pose_landmarks.landmark
+
+        if draw:
+            self.mp_draw.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                self.mp_pose.POSE_CONNECTIONS
+            )
+        return frame, landmarks
