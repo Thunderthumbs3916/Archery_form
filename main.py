e2f0614fed66f20c90c0d96733618529ae590884
Kush Sethia <kushsethia09@gmail.com>
2026-05-30 13:19:57 -0400

    ANchor detction better
    Now with right elbow and shoulder distance too

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import src.pose
 
 running = True
 pose = src.pose.pose()
+cam = src.camera.startVideo()
 
 
-def distance(part1: str, part2: str, landmarks):
-    dx = pose.get_coor(part1, landmarks).x - pose.get_coor((part2, landmarks)).x
-    dy = pose.get_coor(part1, landmarks).y - pose.get_coor((part2, landmarks)).y
+def distance(part1: str, part2: str, landmarks: list, w: int, h: int):
+    dx = (pose.get_coor(part1, landmarks).x * w) - (pose.get_coor(part2, landmarks).x * w)
+    dy = (pose.get_coor(part1, landmarks).y * h) - (pose.get_coor(part2, landmarks).y * h)
     return ((dx**2) + (dy**2))**0.5
 
 
 while True:
-    cam = src.camera.startVideo()
     while running:
         frame = cam.get_frame()
+        h, w = frame.shape[:2]
         if frame is None:
             break
         frame, landmarks = pose.process(frame)
-        print(distance("RShoulder", "RElbow", landmarks))
-        if ((distance("Nose", "RWrist", landmarks)) <= 0.2) :
+
+        #print("shoulder elbow " + str(distance("RShoulder", "RElbow", landmarks, w, h)))
+        print("nose wrist " + str((distance("Nose", "RWrist", landmarks, w, h))))
+        if (distance("Nose", "RWrist", landmarks, w, h)) <= 115 and (distance("RShoulder", "RElbow", landmarks, w, h) > 115):
             cv2.putText(frame, "Anchor", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         cv2.imshow("Video", frame)
         cv2.waitKey(1)
-        #draw
 
         if cv2.getWindowProperty("Video", cv2.WND_PROP_VISIBLE) < 1:
             running = False
